b2844e70c5a05f3fed786c1535ab8091f3491129
viktor1181 <77060337+viktor1181@users.noreply.github.com>
2021-05-06 11:42:50 +0300

    Добавьте файлы проекта.

---
+++ lab2aisd/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace lab2aisd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(531, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "n=";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(556, 22);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(194, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(537, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "input";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(506, 112);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(244, 150);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 22);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Оболочка";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.Name = "Точки";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(454, 311);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ lab2aisd/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace lab2aisd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+        private static bool Ccw(double[] a, double[] b, double[] c)
+        {
+            return ((b[0] - a[0]) * (c[1] - a[1])) > ((b[1] - a[1]) * (c[0] - a[0]));
+        }
+        bool cw(double[] a, double[] b, double[] c)
+        {
+            return a[0] * (b[1] - c[1]) + b[0] * (c[1] - a[1]) + c[0] * (a[1] - b[1]) < 0;
+        }
+
+        bool ccw(double[] a, double[] b, double[] c)
+        {
+            return a[0] * (b[1] - c[1]) + b[0] * (c[1] - a[1]) + c[0] * (a[1] - b[1]) > 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox3.Text);
+            List<double[]> points = new List<double[]>();
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            { 
+                points.Add(new double[2] {
+                    rand.NextDouble() * 100, // ось x
+                    rand.NextDouble() * 100  // ось y
+                });
+            }
+            double min = points[0][0];
+            int minI = 0;
+            // ищем самую левую точку
+            for (var i = 1; i < n; i++)
+            {
+                if (points[i][0] < min)
+                {
+                    min = points[i][0];
+                    minI = i;
+                }
+            }
+            
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (points[j][0] < points[j+1][0] || points[j][0] == points[j + 1][0] && points[j][1] < points[j + 1][1])
+                    {
+                        // меняем элементы местами
+                        var temp = points[j];
+                        points[j] = points[j + 1];
+                        points[j + 1] = temp;
+                    }
+                }
+            }
+
+
+            List<double[]> h = new List<double[]>();
+            List<double[]> up = new List<double[]>();
+            List<double[]> down = new List<double[]>();
+            var p1 = points[0];
+            var p2 = points[points.Count - 1];
+            up.Add(p1);
+            down.Add(p1);
+            for (var i = 1; i < points.Count; ++i)
+            {
+                if (i == points.Count - 1 || cw(p1, points[i], p2))
+                {
+                    while (up.Count >= 2 && !cw(up[up.Count - 2], up[up.Count - 1], points[i]))
+                        up.RemoveAt(up.Count - 1);
+                    up.Add(points[i]);
+                }
+                if (i == points.Count - 1 || ccw(p1, points[i], p2))
+                {
+                    while (down.Count >= 2 && !ccw(down[down.Count - 2], down[down.Count - 1], points[i]))
+                        down.RemoveAt(down.Count - 1);
+                    down.Add(points[i]);
+                }
+            }
+            // lower hull
+            /*
+            foreach (var pt in points)
+            {
+                while (h.Count >= 2 && !Ccw(h[h.Count - 2], h[h.Count - 1], pt))
+                {
+                    h.RemoveAt(h.Count - 1);
+                }
+                h.Add(pt);
+            }
+
+            // upper hull
+            int t = h.Count + 1;
+            for (int i = points.Count - 1; i >= 0; i--)
+            {
+                double[] pt = points[i];
+                while (h.Count >= t && Ccw(h[h.Count - 2], h[h.Count - 1], pt))
+                {
+                    h.RemoveAt(h.Count - 1);
+                }
+                h.Add(pt);
+            }
+
+            h.RemoveAt(h.Count - 1);
+            */
+            BindingSource bindingSource = new BindingSource();
+            bindingSource.DataSource = points.Select(x => new { X = x[0], Y = x[1] }).ToList();
+            dataGridView1.DataSource = bindingSource;
+            for (int i = 0; i < up.Count; ++i)
+                h.Add(up[i]);
+            for (int i = down.Count - 2; i > 0; --i)
+                h.Add(down[i]);
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoom(0, 50);
+            chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+            foreach (var pt in h)
+            {
+                chart1.Series[0].Points.AddXY(pt[0], pt[1]);
+            }
+            chart1.Series[0].Points.AddXY(h[0][0], h[0][1]);
+            chart1.Series[0].Points.AddXY(h[h.Count - 1][0], h[h.Count - 1][1]);
+            foreach (var pt in points)
+            {
+                chart1.Series[1].Points.AddXY(pt[0], pt[1]);
+            }
+            
+
+            points.Clear();
+            h.Clear();
+
+            /*for (int i = 0; i < n; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, Math.Cos(i));
+            }*/
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int[] ch = new int[n];
+/*
+ * StringBuilder stb = new StringBuilder();
+            stb.Append("[");
+            for (int i = 0; i < n; i++)
+            {
+                stb.Append("x:" + points[i][0] + "y:" + points[i][1] + " , ");
+            }
+            stb.Append("]");
+            string result = stb.ToString();
+            textBox1.Text = result;
+ * 
+ * 
+ *   StringBuilder stb1 = new StringBuilder();
+            stb1.Append("[");
+            foreach (var pt in h)
+            {
+                stb1.Append("x:" + pt[0] + "y:" + pt[1] + " , ");
+            }
+            stb1.Append("]");
+            string result1 = stb1.ToString();
+            textBox2.Text = result;
+ * 
+ * 
+ * StringBuilder stb = new StringBuilder();
+stb.Append("[");
+for (int i = 0; i < n; i++)
+{
+    stb.Append(points[0, i] + " , ");
+}
+stb.Append("]");
+string result = stb.ToString();
+textBox1.Text = result;*/
+/*int minI = 0; //номер нижней левой точки
+double min = points[0, 0];
+// ищем нижнюю левую точку
+for (var i = 1; i < n; i++)
+{
+    if (points[0, i] < min)
+    {
+        min = points[0, i];
+        minI = i;
+    }
+}
+// делаем нижнюю левую точку активной
+ch[0] = minI;
+ch[minI] = 0;
+int temp;
+double x1;
+double y1;
+double x2;
+double y2;
+// сортируем вершины в порядке "левизны"
+for (var i = 1; i < minI - 1; i++)
+{
+    for (var j = i + 1; j < minI; j++)
+    {
+        x1 = points[0, ch[0]];
+        y2 = points[1, ch[i]];
+        x2 = points[0, ch[i]];
+        y1 = points[1, ch[0]];
+        double cl = (x2 - x1) * (points[1, ch[j]] - y1) - (y2 - y1) * (points[0, ch[j]] - x1); // векторное произведение.
+        // если векторное произведение меньше 0, следовательно вершина j левее вершины i.Меняем их местами
+        if (cl < 0)
+        {
+            temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+        }
+    }
+    //записываем в стек вершины, которые точно входят в оболочку
+    Stack<double> h = new Stack<double>();
+    h.Push(ch[0]);
+    h.Push(ch[1]);
+
+    x1 = points[0, ch[0]];
+    y2 = points[1, ch[i]];
+    x2 = points[0, ch[i]];
+    y1 = points[1, ch[0]];
+
+    for (int i = 2; i < minI; i++)
+    {
+        while ()
+        {
+            h.Pop(); // пока встречается правый поворот, убираем точку из оболочки
+        }
+        h.Push(ch[i]); // добавляем новую точку в оболочку
+    }*/
